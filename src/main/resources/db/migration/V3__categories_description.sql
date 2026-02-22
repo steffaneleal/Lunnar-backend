26b1edc8a6f58462fc0e7d02989d3f5f43af1bc9
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2026-02-22 14:42:59 -0300

    :wastebasket: remove: removendo o flyway

--- src/main/resources/db/migration/V3__categories_description.sql
+++
@@ -1,5 +0,0 @@
--- V3: Adiciona a coluna de endereço de entrega à tabela de pedidos
-ALTER TABLE tb_order ADD COLUMN shipping_address_id UUID;
-
--- Adiciona a restrição de chave estrangeira
-ALTER TABLE tb_order ADD CONSTRAINT fk_order_shipping_address FOREIGN KEY (shipping_address_id) REFERENCES tb_address(id);
